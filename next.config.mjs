a20d4f7b82eb4a11e9db2afaf5788e5aa2cb80f1
Daniel Scharp <daniel.scharp@hotmail.com>
2026-09-17 11:57:06 -0300

    retirando alterações de teste

--- next.config.mjs
+++ next.config.mjs
@@ -6,13 +6,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  output: 'standalone',
-  // Força o bundling do pacote SWC no servidor
-  serverExternalPackages: [],
-  transpilePackages: ['@swc/helpers'],
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 }
 
 export default nextConfig
